a0d440c60f32ef28e88609feff7b23767526efe8
imadharsh <31060638+imadharsh@users.noreply.github.com>
2017-08-16 19:34:34 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -9,7 +9,7 @@ element.innerhtml = 'New value';
 var img = document.getElementById('madi');
 var marginleft = 0;
 function moveRight (){
-    marginLeft = marginLeft = 5;
+    marginLeft = marginLeft = 10;
     img.style.marginLeft = marginLeft + 'px';
 }
 img.onclick = function() {
